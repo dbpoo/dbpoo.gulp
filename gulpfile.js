af4b546d889381724ade76e88e5f14c2938bc810
dbpoo <weiwei198328@gmail.com>
2014-03-07 15:13:03 +0800

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /**
  * 初始化
- * npm install gulp-util gulp-imagemin gulp-sass gulp-minify-css gulp-uglify gulp-rename gulp-concat gulp-clean gulp-clean tiny-lr --save-dev
+ * npm install gulp-util gulp-imagemin gulp-ruby-sass gulp-minify-css gulp-jshint gulp-uglify gulp-rename gulp-concat gulp-clean gulp-livereload tiny-lr --save-dev
  */
 
 // 引入 gulp及组件
@@ -8,7 +8,7 @@ var gulp    = require('gulp'),                 //基础库
     imagemin = require('gulp-imagemin'),       //图片压缩
     sass = require('gulp-ruby-sass'),          //sass
     minifycss = require('gulp-minify-css'),    //css压缩
-    jshint = require('gulp-jshint'),           //js检查
+    //jshint = require('gulp-jshint'),           //js检查
     uglify  = require('gulp-uglify'),          //js压缩
     rename = require('gulp-rename'),           //重命名
     concat  = require('gulp-concat'),          //合并文件
@@ -58,8 +58,8 @@ gulp.task('js', function () {
         jsDst ='./dist/js';
 
     gulp.src(jsSrc)
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('default'))
+        //.pipe(jshint('.jshintrc'))
+        //.pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
         .pipe(gulp.dest(jsDst))
         .pipe(rename({ suffix: '.min' }))
